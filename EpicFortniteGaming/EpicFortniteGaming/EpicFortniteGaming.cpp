e4b2f86e0257422215c56c15d14a993ff8f7f726
Professional Puppy <professionalkateg@gmail.com>
2023-07-31 21:40:28 +0100

    Added a private block with a string price. Then created a getter/setter combo in the public block

--- EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
+++ EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
@@ -7,14 +7,30 @@ public:
 
     chair()
     {
+        // interestingly, a call to (c|m)alloc won't construct an object
         std::cout << "Making chair!" << "\n";
         this->legs = 200;
     }
 
     ~chair()
     {
+        // interestingly once more, a free() call won't cause a destructor to run
         std::cout << "Removing chair!" << "\n";
+        // meaning, generic C things should stay away from C++
     }
+
+    std::string get_price()
+    {
+        return this->price_;
+    }
+
+    void set_price(std::string price)
+    {
+        this->price_ = price;
+    }
+
+private:
+    std::string price_;
 };
 
 int main()
@@ -22,6 +38,11 @@ int main()
     chair* awesome_chair = new chair();
     awesome_chair->legs = 10;
     std::cout << awesome_chair->legs << "\n";
+
+    awesome_chair->set_price("10 Banana");
+    std::string chair_price = awesome_chair->get_price();
+    std::cout << chair_price << "\n";
+    
     delete awesome_chair; 
 
     return 0;
