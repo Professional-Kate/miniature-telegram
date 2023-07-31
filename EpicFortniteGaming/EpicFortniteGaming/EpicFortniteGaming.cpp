38caea073b85dc5ba1ae08cb32a8de5efbdc5ee6
Professional Puppy <professionalkateg@gmail.com>
2023-07-31 21:26:22 +0100

    we love constructors and destructors, we really love them!

--- EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
+++ EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
@@ -4,19 +4,25 @@ class chair
 {
 public:
     int legs;
+
+    chair()
+    {
+        std::cout << "Making chair!" << "\n";
+        this->legs = 200;
+    }
+
+    ~chair()
+    {
+        std::cout << "Removing chair!" << "\n";
+    }
 };
 
 int main()
 {
-    chair* awesome_chair = new chair(); // new must call malloc 
+    chair* awesome_chair = new chair();
     awesome_chair->legs = 10;
-    free(awesome_chair); // because it has to be freed
-    std::cout << awesome_chair->legs << "\n"; // prints gabagool to the stdout - exit code 0
+    std::cout << awesome_chair->legs << "\n";
+    delete awesome_chair; 
 
-    chair* heap_chair = (chair*) calloc(1, sizeof(chair)); // messy af line when using (m|c)alloc
-    heap_chair->legs = 24;
-    delete heap_chair;
-    std::cout << heap_chair->legs << "\n"; // unexpected exit code for program - exit code -1,073,741,819
-    
     return 0;
 }
