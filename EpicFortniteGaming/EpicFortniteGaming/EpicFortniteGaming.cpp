03a4b4db1629cdb8dffd30523aec9d3572c2dfe4
Professional Puppy <professionalkateg@gmail.com>
2023-07-31 21:17:41 +0100

    the commit where I write gabagool in a comment

--- EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
+++ EpicFortniteGaming/EpicFortniteGaming/EpicFortniteGaming.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
-
-int main(int argc, char* argv[])
+class chair
 {
-    int* int_pointer = (int*) calloc(0, sizeof(int));
-    *int_pointer = 10;
+public:
+    int legs;
+};
 
-    std::cout << *int_pointer;
+int main()
+{
+    chair* awesome_chair = new chair(); // new must call malloc 
+    awesome_chair->legs = 10;
+    free(awesome_chair); // because it has to be freed
+    std::cout << awesome_chair->legs << "\n"; // prints gabagool to the stdout - exit code 0
+
+    chair* heap_chair = (chair*) calloc(1, sizeof(chair)); // messy af line when using (m|c)alloc
+    heap_chair->legs = 24;
+    delete heap_chair;
+    std::cout << heap_chair->legs << "\n"; // unexpected exit code for program - exit code -1,073,741,819
+    
     return 0;
 }
