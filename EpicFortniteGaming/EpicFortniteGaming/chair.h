589607807f3e4f6072a3aa41840e4b52b1ebf6a5
Professional Puppy <professionalkateg@gmail.com>
2023-07-31 22:03:58 +0100

    doc comment. I would do more but I finna sleep

--- EpicFortniteGaming/EpicFortniteGaming/chair.h
+++ EpicFortniteGaming/EpicFortniteGaming/chair.h
@@ -7,8 +7,17 @@ public:
     chair();
     ~chair();
 
+    /**
+     * \brief Gets the price of this chair!
+     * \return string price of this chair.
+     */
     std::string get_price();
-    void set_price(std::string);
+
+    /**
+     * \brief sets the price of this chair.
+     * \param price the new price of the chair.
+     */
+    void set_price(std::string price);
 
 private:
     std::string price_;
